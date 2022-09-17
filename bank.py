7a3076bbe365d9c6144038fbf9b6421cac19b0d9
nammm <113276920+knamm@users.noreply.github.com>
2022-09-17 22:07:08 +0700

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,10 @@
+# Tugas 1 If, else, dan elif
+ 
+greet=input("Your greet : ")
+
+if greet=="Hello" or greet=="Hello, Newman":
+    print("$0")
+elif greet=="How you doing?":
+    print("$20")
+elif greet=="What's happening?":
+    print("$100")
